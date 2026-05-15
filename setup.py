1c39df4504e5a6058db49bfcbc0c20eebbacfb3c
SOCBotMaster <socbotmaster@guardiankey.io>
2026-05-15 18:56:02 -0300

    Update package requirements in setup.py

--- setup.py
+++ setup.py
@@ -21,12 +21,10 @@ VERSION = "1.1.7RC1"
 # http://pypi.python.org/pypi/setuptools
 
 REQUIRES = [
-    "certifi >= 14.5.14",
-    "frozendict ~= 2.3.4",
-    "python-dateutil >=2.7,<3",
-    "setuptools >= 21.0.0",
-    "typing_extensions ~= 4.3.0",
-    "urllib3 ~= 1.26.7",
+    "certifi",
+    "frozendict>=2.3,<3",
+    "python-dateutil>=2.8,<3",
+    "urllib3>=1.26,<3",
 ]
 
 setup(
